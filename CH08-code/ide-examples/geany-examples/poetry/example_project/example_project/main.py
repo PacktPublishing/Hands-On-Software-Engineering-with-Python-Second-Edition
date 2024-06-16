e2835f53c0ccc6b15be9738a282c2ca89ec30977
Brian Allbee <brian.allbee@gmail.com>
2024-06-15 18:04:46 -0600

    Example Project with Geany and Poetry

---
+++ CH08-code/ide-examples/geany-examples/poetry/example_project/example_project/main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3.11
+"""
+A bare-bones project module. All this module does is print that
+it's being run, this docstring, and the interpreter that it's
+running under.
+"""
+
+import os
+import platform
+
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+if __name__ == '__main__':
+    from shutil import which
+    interpreter = which('python')
+    print(f'Running {__file__}')
+    print('Example using {PACKAGE_MANAGER} in {IDE}'.format(**os.environ))
+    print(f'The requests module: {requests}')
+    print(__doc__)
+    print(f'Project Python interpreter: {interpreter}')
+
+    if platform.system() == 'Windows':
+        input('Run completed without error. Press [Enter] to continue')

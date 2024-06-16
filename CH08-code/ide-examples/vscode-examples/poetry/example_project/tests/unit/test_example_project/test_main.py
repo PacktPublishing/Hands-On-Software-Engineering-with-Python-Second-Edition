bdb5852a211924347c92775899b4ebe324cc4352
Brian Allbee <brian.allbee@gmail.com>
2024-06-15 18:10:05 -0600

    Example Project with VS Code and Poetry

---
+++ CH08-code/ide-examples/vscode-examples/poetry/example_project/tests/unit/test_example_project/test_main.py
@@ -0,0 +1,23 @@
+"""
+A dummy test-module, created so that a test-suite can be executed with
+
+pipenv run pytest tests/unit
+
+This module can also be run itself, assuming that the IDE supports that.
+"""
+
+import pytest
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from example_project.main import CONSTANT
+
+
+def test_nothing():
+    """A dummy test, so that pytest can run without error"""
+    assert CONSTANT
+
+
+if __name__ == '__main__':
+    pytest.main([__file__, '-vv'])

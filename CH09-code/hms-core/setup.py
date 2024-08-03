38f9878159d6d448b8d5a47eec5f130f97aa64fb
Brian Allbee <brian.allbee@gmail.com>
2024-08-03 10:13:20 -0600

    Linted and (mostly) clean copy of original code, Ch. 10 restructure

---
+++ CH09-code/hms-core/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+import sys
+sys.path.append('../standards')
+sys.path.append('tests/test_hms_core')
+
+# The actual setup function call:
+setup(
+    name='HMS-Core',
+    version='0.1.dev0',
+    author='Brian D. Allbee',
+    description='',
+    package_dir={
+        '': 'src',
+        # ...
+    },
+    # Can also be automatically generated using
+    #     setuptools.find_packages...
+    packages=[
+        'hms_core',
+        # ...
+    ],
+    package_data={
+        # 'hms_core': [
+           # 'filename.ext',
+           # ...
+        # ]
+    },
+    entry_points={
+        # 'console_scripts': [
+            # 'executable_name = namespace.path: function',
+            # ...
+        # ],
+    },
+    test_suite='tests.test_hms_core',
+)

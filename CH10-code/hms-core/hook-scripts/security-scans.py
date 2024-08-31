fd04cf51b67318c2c89752b6407f611390aca875
Brian Allbee <brian.allbee@gmail.com>
2024-08-31 11:52:38 -0600

    Adds hook-scripts and security-scans.py examples
    Updates security-scan to non-deprecated subprocess.run instead of call

---
+++ CH10-code/hms-core/hook-scripts/security-scans.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3.11
+"""
+A script that runs pipenv check with a collection of
+vulnerabilities to ignore, each of which is documented
+with a reason in the script itself.
+
+See example-scan-results.txt for details
+"""
+import os
+import sys
+
+from subprocess import call
+from shutil import which
+
+assert which('pipenv'), \
+    f'{__file__.split(os.sep)[-1].split(os.extsep)[0]} ' \
+    'requires an active pipenv environment, but the ' \
+    'pipenv command could not be found.'
+
+IGNORE_ITEMS = {
+    # vulnerability_number: 'Reason to ignore it',
+
+    # 39611: 'Not deployed to production, so not a '
+    # 'critical issue'
+}
+
+command = ['pipenv', 'check'] + [
+    f'-i {number}' for number in IGNORE_ITEMS
+]
+
+print(f'Running {" ".join(command)}')
+sys.exit(call(' '.join(command), shell=True))

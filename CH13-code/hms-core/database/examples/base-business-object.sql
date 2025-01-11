522870e6f9ad2a66b58609d6cb014fcfd6364316
Brian Allbee <brian.allbee@gmail.com>
2025-01-11 14:27:03 -0700

    Add BaseBusinessObject table example (not intended for actual use!)
    Add initial BaseDataObject ABC definition
    
    Clean up the BaseBusinessObject example table definition
    
    Add template.env file for developer use
    
    Remove temporary inline testing in hms.core.data_objects
    
    Add schema-generator script for BaseDataObject, initial output
    
    Add package dependencies for BaseDataObject and related

---
+++ CH13-code/hms-core/database/examples/base-business-object.sql
@@ -0,0 +1,20 @@
+CREATE TABLE BaseBusinessObject (
+    oid CHAR(36) NOT NULL
+    COMMENT 'The unique identifier of the record; a UUID value (for example b279fda6-eafc-40e5-b8c7-415ed864acf7).',
+    is_active TINYINT(1) DEFAULT 1 NOT NULL
+    COMMENT 'Flag indicating whether the object state-data is active (1/True) or not (0/False)',
+    is_deleted tinyint(1) DEFAULT 0 NOT NULL
+    COMMENT 'Flag indicating whether the object state-data is considered "deleted," and thus not accessible (1/True) or not (0/False)',
+    created DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL
+    COMMENT 'The UTC date/time that the record was originally created.',
+    modified DATETIME ON UPDATE CURRENT_TIMESTAMP NULL
+    COMMENT 'The UTC date/time that the record was last modified.',
+    -- IMPORTANT: This PK constraint will need to be
+    -- renamed in derived table definitions!
+    CONSTRAINT BaseBusinessObject_PK PRIMARY KEY (oid)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf8mb4_unicode_ci
+COMMENT='A reference table, showing common fields and their definitions for other tables that will store business object state-data.'
+;
--- CH13-code/hms-core/src/hms/core/data_objects.py
+++ CH13-code/hms-core/src/hms/core/data_objects.py
@@ -7,12 +7,18 @@ is persisted in a back-end data-store.
 from __future__ import annotations
 
 # Built-In Imports
+import abc
 import os
 
+from datetime import datetime
 from functools import cache
+from typing import Any
+from uuid import UUID, uuid4
 
 # Third-Party Imports
 import mysql.connector
+
+from pydantic import BaseModel, Field
 
 from mysql.connector.connection_cext import CMySQLConnection
 
@@ -66,7 +72,78 @@ def get_env_database_connector() -> CMySQLConnection:
 
 # Module Metaclasses
 
+
 # Module Abstract Base Classes
+class BaseDataObject(metaclass=abc.ABCMeta):
+    """
+    Provides baseline functionality, interface requirements and type identity for objects that can persist their state data in a back-end RDBMS data store.
+    """  # noqa: E501
+
+    oid: UUID = Field(
+        title='Object ID',
+        description='The unique identifier of the record '
+        'for the instance\'s state data in the back end '
+        'data store',
+        examples=[
+            UUID('0'*32),
+            '073f2f01-64b6-441f-a053-b3aaa3cf5a1e',
+            UUID('f'*32),
+        ],
+        default_factory=uuid4, frozen=True,
+    )
+    is_active: bool = Field(
+        title='Is Active',
+        description='Flag indicating whether the object '
+        'is "active."',
+        examples=[True, False],
+        default=False
+    )
+    is_deleted: bool = Field(
+        title='Is Deleted',
+        description='Flag indicating whether the object '
+        'is "deleted" (pending an **actual** deletion '
+        'later, perhaps).',
+        examples=[True, False],
+        default=False,
+    )
+    created: datetime = Field(
+        title='Created Date',
+        description='The date/time (UTC) when the object '
+        'was created.',
+        examples=['2025-01-04T14:52:39.842206'],
+        default_factory=datetime.utcnow, frozen=True,
+    )
+    modified: datetime | None = Field(
+        title='Last Modified Date',
+        description='The (optional) date/time (UTC) when '
+        'the object was last modified.',
+        examples=[None, '2025-01-04T14:52:39.842206'],
+        default=None,
+    )
+
+    @classmethod
+    def from_record(
+        cls,
+        data: dict[str, Any] | tuple[tuple[str, Any]]
+    ) -> BaseModel:
+        """
+        Creates and returns an instance of the class from
+        either of the common record-structure return-types
+        from a back-end data-store.
+
+        Parameters:
+        -----------
+        data : dict, or tuple of tuples
+            The state representation of the object to
+            create from query results against a data
+            store. Expected to be either a dict of field
+            names and values, or a tuple of key/value
+            tuples that can be converted to such a dict
+        """
+        if isinstance(data, tuple):
+            data = dict(data)
+        return cls(**data)
+
 
 # Module Concrete Classes
 
@@ -74,10 +151,4 @@ def get_env_database_connector() -> CMySQLConnection:
 # Code to run if the module is executed directly
 if __name__ == '__main__':
 
-    with get_env_database_connector() as db:
-        with db.cursor() as cursor:
-            cursor.execute('SHOW TABLES;')
-            print(
-                'SHOW TABLES results: '
-                f'{cursor.fetchall()}'
-            )
+    pass

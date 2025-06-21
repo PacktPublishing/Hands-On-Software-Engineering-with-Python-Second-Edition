b729baf9d86d74b1314ec785f3e985504c465c82
Brian Allbee <brian.allbee@gmail.com>
2025-06-21 13:47:20 -0600

    Add common code
    Add Lambda Function stubs
    
    Add local_api for local development/testing
    
    Add initial package dependencies for chapter code
    
    Add standard project template.env
    
    Correct dumb typo in all the stubs, add separate stub module

---
+++ CH17-code/src/admin_read_artisan/admin_read_artisan.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3.11
+"""
+Provides the backing Lambda Function for the API endpoint
+that handles read artisan functionality.
+"""
+from __future__ import annotations
+
+# Built-In Imports
+import json
+
+from pathlib import Path
+
+# Third-Party Imports
+from awslambdaric.lambda_context import LambdaContext
+from goblinfish.metrics.trackers import ProcessTracker
+
+# Path Manipulations (avoid these!) and "Local" Imports
+from logger import logger
+
+# Module "Constants" and Other Attributes
+tracker = ProcessTracker()
+
+module = Path(__file__).stem
+
+LambdaProxyInput = dict[str, str]
+LambdaProxyOutput = dict[str, str]
+
+# Lambda Handlers
+
+@tracker
+def api_handler(
+    event: LambdaProxyInput, context: LambdaContext
+) -> LambdaProxyOutput:
+    """
+    The read handler for artisan targets
+    in the admin scope.
+
+    Parameters:
+    -----------
+    event : LambdaProxyInput (dict)
+        The API event to be handled.
+    context : LambdaContext
+        The standard Lambda context object provided
+        by AWS during a Lambda invocation.
+    """
+    try:
+        logger.info(f'{module}.api_handler called')
+        logger.debug(f'event: {json.dumps(event)}')
+        logger.debug(f'context: {repr(context)}')
+        # TODO: Replace this with actual logic
+        result = {
+            'statusCode': 501,
+            'body': 'Not Implemented '
+            f'({context.aws_request_id})'
+        }
+
+    # TODO: Add other exception-handling if needed
+
+    except Exception as error:
+        logger.exception(
+            f'{error.__class__.__name__}: {error} '
+            'occured in api_handler'
+        )
+        logger.error(f'event: {json.dumps(event)}')
+        logger.error(f'context: {repr(context)}')
+        result = {
+            'statusCode': 500,
+            'body': 'Internal Server Error: '
+            f'({context.aws_request_id})'
+        }
+
+    logger.info(f'{module}.api_handler complete')
+    logger.debug(f'result: {json.dumps(result)}')
+    return result
+
+
+# Helper Functions
+
+# Module Metaclasses (if any)
+
+# Module Abstract Base Classes (if any, requires abc)
+
+# Module Concrete Classes
+
+# Code to run if the module is executed directly
+
+if __name__ == '__main__':
+
+    import logging
+
+    from os import sep, extsep
+
+    formatter = logging.Formatter(
+        "[%(levelname)s]  %(message)s"
+    )
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    module = __file__.split(sep)[-1].split(extsep)[0]
+    logger.info(f'Running {module}.__main__')
+    result = api_handler({}, None)
+    logger.info(
+        'api_handler result: '
+        f'{json.dumps(result, indent=4)}'
+    )
+    logger.info(f'{module}.__main__ completed')

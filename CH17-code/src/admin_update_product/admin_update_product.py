ec4acfd451345ce0a829e79cc2c9d631e0efb498
Brian Allbee <brian.allbee@gmail.com>
2025-07-19 09:01:38 -0600

    Implement Admin Update Product endpoint
    Add error handling

--- CH17-code/src/admin_update_product/admin_update_product.py
+++ CH17-code/src/admin_update_product/admin_update_product.py
@@ -15,6 +15,8 @@ from awslambdaric.lambda_context import LambdaContext
 from goblinfish.metrics.trackers import ProcessTracker
 
 # Path Manipulations (avoid these!) and "Local" Imports
+from hms.core.business_objects import Product, \
+    ProductNotFoundError
 from logger import logger
 
 # Module "Constants" and Other Attributes
@@ -47,14 +49,128 @@ def api_handler(
         logger.info(f'{module}.api_handler called')
         logger.debug(f'event: {json.dumps(event)}')
         logger.debug(f'context: {repr(context)}')
-        # TODO: Replace this with actual logic
+
+        # Get the oid of the Product to be updated
+        product_oid = event.get(
+            'pathParameters', {}
+        ).get('oid')
+        if product_oid is None \
+                or len(product_oid.split(',')) != 1:
+            raise ValueError(
+                f'{module}.api_handler requires a single '
+                'oid, but that path parameter resolved to '
+                f'"{product_oid}" ({type(product_oid).__name__}).'
+            )
+
+        # Retrieve the payload from event['body']
+        body = payload = json.loads(
+            event.get('body', 'null')
+        )
+        logger.debug(f'payload: {json.dumps(payload)}')
+
+        if payload is None:
+            raise ValueError(
+                f'{module}.api_handler expects one or '
+                'more allowed fields with values to '
+                'update, but none were supplied.'
+            )
+
+        # Check for allowed fields in the update request
+        allowed_fields = {'is_active', 'is_deleted'}
+        payload = {
+            key: payload[key] for key in allowed_fields
+            if key in payload
+        }
+        if not payload:
+            raise ValueError(
+                f'{module}.api_handler expects one or '
+                'more allowed fields '
+                f'{tuple(allowed_fields)} with values'
+                ' to update, but none were supplied.'
+            )
+
+        # If the request has fields that are not
+        # allowed, raise an error
+        requested_fields = set(body.keys())
+        logger.debug(
+            f'requested_fields: {requested_fields}'
+        )
+        forbidden_fields = requested_fields.difference(
+            allowed_fields
+        )
+        logger.debug(
+            f'forbidden_fields: {forbidden_fields}'
+        )
+        if forbidden_fields:
+            raise ValueError(
+                f'{module}.api_handler was passed '
+                f'{tuple(forbidden_fields)} fields, '
+                'which are not allowed in an update.'
+            )
+
+        # Retrieve the current Product and convert
+        # it to a dict
+        _authnz_preflight()
+        with tracker.timer('product_db_read_access'):
+            products = Product.get(
+                product_oid, db_source_name='Products'
+            )
+        # Raise an error if no Product could be found
+        if len(products) == 0:
+            raise ProductNotFoundError(
+                'Could not retrieve an Product '
+                f'identified by "{product_oid}".'
+            )
+        product_data = products[0].model_dump(mode='json')
+        logger.debug(
+            f'Unmodified Product {product_oid}: '
+            f'{json.dumps(product_data)}'
+        )
+        # Update that dict with the payload values
+        product_data.update(payload)
+        logger.debug(
+            f'Modified Product {product_oid}: '
+            f'{json.dumps(product_data)}'
+        )
+        _authnz_reconcile()
+        # Create a new Product instance with the
+        # updated data and save it
+        updated_product = Product(**product_data)
+        with tracker.timer('product_db_write_access'):
+            updated_product.save(db_source_name='Products')
+
         result = {
-            'statusCode': 501,
-            'body': 'Not Implemented '
+            'statusCode': 200,
+            'body': json.dumps(
+                updated_product.model_dump(mode='json')
+            )
+        }
+
+    except ProductNotFoundError as error:
+        logger.exception(
+            f'{error.__class__.__name__}: {error} '
+            'occured in api_handler'
+        )
+        logger.error(f'event: {json.dumps(event)}')
+        logger.error(f'context: {repr(context)}')
+        result = {
+            'statusCode': 404,
+            'body': 'Not Found: '
             f'({context.aws_request_id})'
         }
 
-    # TODO: Add other exception-handling if needed
+    except ValueError as error:
+        logger.exception(
+            f'{error.__class__.__name__}: {error} '
+            'occured in api_handler'
+        )
+        logger.error(f'event: {json.dumps(event)}')
+        logger.error(f'context: {repr(context)}')
+        result = {
+            'statusCode': 400,
+            'body': 'Bad Request: '
+            f'({context.aws_request_id})'
+        }
 
     except Exception as error:
         logger.exception(
@@ -75,6 +191,13 @@ def api_handler(
 
 
 # Helper Functions
+def _authnz_preflight(*args, **kwargs):
+    ...
+
+
+def _authnz_reconcile(*args, **kwargs):
+    ...
+
 
 # Module Metaclasses (if any)
 
